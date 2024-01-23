47b3e2aede243298ba0b129c314ff9a5145ba29e
Brianca Knight <briancaknight@gmail.com>
2024-01-23 15:26:09 -0800

    add RESTful routing

--- ToDos/Controllers/ItemsController.cs
+++ ToDos/Controllers/ItemsController.cs
@@ -14,7 +14,7 @@ namespace ToDos.Controllers
     }
 
     [HttpGet("/items/new")]
-    public ActionResult CreateForm()
+    public ActionResult New()
     {
       return View();
     }
@@ -31,6 +31,13 @@ namespace ToDos.Controllers
     {
       Item.ClearAll();
       return View();
+    }
+
+     [HttpGet("/items/{id}")]
+    public ActionResult Show(int id)
+    {
+      Item foundItem = Item.Find(id);
+      return View(foundItem);
     }
   }
 }
