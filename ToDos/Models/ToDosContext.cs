95a706a9e172bda00d557ffed4da9e9e852a1934
Brianca Knight <briancaknight@gmail.com>
2024-01-29 15:39:10 -0800

    WIP estabilsh one-to-many relationship with categories controller

--- ToDos/Models/ToDosContext.cs
+++ ToDos/Models/ToDosContext.cs
@@ -4,6 +4,7 @@ namespace ToDos.Models
 {
   public class ToDosContext : DbContext
   {
+    public DbSet<Category> Categories { get; set; }
     public DbSet<Item> Items { get; set; }
 
     public ToDosContext(DbContextOptions options) : base(options) { }
